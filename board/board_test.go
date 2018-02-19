ce4ba11159c21cf2722795237a34080d0a7b5d22
Michael Moore <michael.moore@contrastsecurity.com>
2018-02-19 13:01:23 -0500

    Test that board pieces are not nil

--- board/board_test.go
+++ board/board_test.go
@@ -3,13 +3,18 @@ package board_test
 import (
 	"testing"
 
+	"github.com/faiface/pixel/imdraw"
 	"github.com/miketmoore/go-chess/board"
 	"golang.org/x/image/colornames"
 )
 
 func TestBuild(t *testing.T) {
-	squares := board.Build(50, colornames.Black, colornames.White)
-	if len(squares) != 64 {
-		t.Fatal("Total squares is unexpected")
+	var squares [64]*imdraw.IMDraw
+	squares = board.Build(50, colornames.Black, colornames.White)
+
+	for _, square := range squares {
+		if square == nil {
+			t.Fatal("square is nil")
+		}
 	}
 }
