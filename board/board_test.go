17ac983e4f03d937f7c7457a1a5e204f7ed6cd47
Michael Moore <michael.moore@contrastsecurity.com>
2018-02-19 23:37:52 -0500

    Update board test

--- board/board_test.go
+++ board/board_test.go
@@ -3,18 +3,15 @@ package board_test
 import (
 	"testing"
 
-	"github.com/faiface/pixel/imdraw"
 	"github.com/miketmoore/go-chess/board"
 	"golang.org/x/image/colornames"
 )
 
 func TestBuild(t *testing.T) {
-	var squares [64]*imdraw.IMDraw
-	squares = board.Build(50, colornames.Black, colornames.White)
+	var squares board.Map
+	squares = board.Build(0, 0, 50, colornames.Black, colornames.White)
 
-	for _, square := range squares {
-		if square == nil {
-			t.Fatal("square is nil")
-		}
+	if len(squares) != 64 {
+		t.Fatal("not enough squares")
 	}
 }
