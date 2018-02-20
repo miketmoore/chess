9da439252639cc46da6d69f456d6ca893476b750
Michael Moore <michael.moore@contrastsecurity.com>
2018-02-19 20:51:36 -0500

    Make var internal

--- board/board.go
+++ board/board.go
@@ -12,20 +12,20 @@ const totalSquares = 64
 const totalRows = 8
 const totalCols = 8
 
-type Board map[string]*imdraw.IMDraw
+type boardMap map[string]*imdraw.IMDraw
 
 // Build returns an array of *imdraw.IMDraw instances, each representing one square
 // on a chess board. The size argument defines the width and height of each square.
 // The blackFill and whiteFill arguments define what colors are used for the "black"
 // and "white" squares.
-func Build(size float64, blackFill, whiteFill color.RGBA) Board {
+func Build(size float64, blackFill, whiteFill color.RGBA) boardMap {
 	var squareW float64 = size
 	var squareH float64 = size
 	var r, c float64
-	squares := Board{}
 	var colorFlag = true
 	var xInc, yInc float64
 	i := 0
+	squares := boardMap{}
 
 	colNames := [totalCols]string{"a", "b", "c", "d", "e", "f", "g", "h"}
 
