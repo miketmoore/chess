616f840cd89d0e8f6ae08546484c3a8d01b8a404
Michael Moore <michael.moore@contrastsecurity.com>
2018-02-20 00:14:06 -0500

    Use struct instead of map

--- pieces/pieces.go
+++ pieces/pieces.go
@@ -17,8 +17,18 @@ type ByColor map[string]ByName
 
 // Drawer contains all chess piece sprites
 type Drawer struct {
-	Black ByName
-	White ByName
+	Black Set
+	White Set
+}
+
+// Set contains one sprite per type of piece
+type Set struct {
+	King   *pixel.Sprite
+	Queen  *pixel.Sprite
+	Bishop *pixel.Sprite
+	Knight *pixel.Sprite
+	Rook   *pixel.Sprite
+	Pawn   *pixel.Sprite
 }
 
 // New constructs a ByColor (chess piece sprites)
@@ -30,21 +40,21 @@ func New() Drawer {
 	}
 
 	return Drawer{
-		Black: ByName{
-			"king":   newSprite(pic, 0, 0, 40, 40),
-			"queen":  newSprite(pic, 40, 0, 90, 40),
-			"bishop": newSprite(pic, 90, 0, 140, 40),
-			"knight": newSprite(pic, 130, 0, 180, 40),
-			"rook":   newSprite(pic, 185, 0, 220, 40),
-			"pawn":   newSprite(pic, 230, 0, 270, 40),
+		Black: Set{
+			King:   newSprite(pic, 0, 0, 40, 40),
+			Queen:  newSprite(pic, 40, 0, 90, 40),
+			Bishop: newSprite(pic, 90, 0, 140, 40),
+			Knight: newSprite(pic, 130, 0, 180, 40),
+			Rook:   newSprite(pic, 185, 0, 220, 40),
+			Pawn:   newSprite(pic, 230, 0, 270, 40),
 		},
-		White: ByName{
-			"king":   newSprite(pic, 0, 40, 40, 85),
-			"queen":  newSprite(pic, 40, 40, 90, 85),
-			"bishop": newSprite(pic, 90, 40, 140, 85),
-			"knight": newSprite(pic, 130, 40, 185, 85),
-			"rook":   newSprite(pic, 185, 40, 220, 85),
-			"pawn":   newSprite(pic, 230, 40, 270, 85),
+		White: Set{
+			King:   newSprite(pic, 0, 40, 40, 85),
+			Queen:  newSprite(pic, 40, 40, 90, 85),
+			Bishop: newSprite(pic, 90, 40, 140, 85),
+			Knight: newSprite(pic, 130, 40, 185, 85),
+			Rook:   newSprite(pic, 185, 40, 220, 85),
+			Pawn:   newSprite(pic, 230, 40, 270, 85),
 		},
 	}
 }
