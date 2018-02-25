5ece3dfb1a3fc6a9d6f6eba1807438d7ef4c4b68
Michael Moore <michael.moore@contrastsecurity.com>
2018-02-25 09:53:51 -0500

    Add player Color

---
+++ player/player.go
@@ -0,0 +1,11 @@
+package player
+
+// Color is a type for player colors
+type Color string
+
+const (
+	// White represents the white player
+	White Color = "white"
+	// Black represents the black player
+	Black Color = "black"
+)
