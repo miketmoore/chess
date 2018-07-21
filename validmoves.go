bacda10ad3edc72670ac4e527ab8455916c75b60
Michael Moore <michael.moore@contrastsecurity.com>
2018-07-20 23:49:57 -0400

    :recycle: make code more reusable

--- validmoves.go
+++ validmoves.go
@@ -22,10 +22,114 @@ func isCoordStartPosition(playerColor PlayerColor, piece Piece, rank, file strin
 	return false
 }
 
+// Is the rank n spaces north a real board location?
 func getRankAhead(rank string, n int) (string, bool) {
 	if s, err := strconv.Atoi(rank); err == nil {
 		fmt.Printf("%T, %v", s, s)
 		return fmt.Sprintf("%d", s+n), true
+	}
+	return "", false
+}
+
+type Direction string
+
+const (
+	North Direction = "north"
+)
+
+var ranks = []int{1, 2, 3, 4, 5, 6, 7, 8}
+var files = []string{"a", "b", "c", "d", "e", "f", "g", "h"}
+
+var validCoords = map[string]bool{
+	"a1": true,
+	"a2": true,
+	"a3": true,
+	"a4": true,
+	"a5": true,
+	"a6": true,
+	"a7": true,
+	"a8": true,
+
+	"b1": true,
+	"b2": true,
+	"b3": true,
+	"b4": true,
+	"b5": true,
+	"b6": true,
+	"b7": true,
+	"b8": true,
+
+	"c1": true,
+	"c2": true,
+	"c3": true,
+	"c4": true,
+	"c5": true,
+	"c6": true,
+	"c7": true,
+	"c8": true,
+
+	"d1": true,
+	"d2": true,
+	"d3": true,
+	"d4": true,
+	"d5": true,
+	"d6": true,
+	"d7": true,
+	"d8": true,
+
+	"e1": true,
+	"e2": true,
+	"e3": true,
+	"e4": true,
+	"e5": true,
+	"e6": true,
+	"e7": true,
+	"e8": true,
+
+	"f1": true,
+	"f2": true,
+	"f3": true,
+	"f4": true,
+	"f5": true,
+	"f6": true,
+	"f7": true,
+	"f8": true,
+
+	"g1": true,
+	"g2": true,
+	"g3": true,
+	"g4": true,
+	"g5": true,
+	"g6": true,
+	"g7": true,
+	"g8": true,
+
+	"h1": true,
+	"h2": true,
+	"h3": true,
+	"h4": true,
+	"h5": true,
+	"h6": true,
+	"h7": true,
+	"h8": true,
+}
+
+func coordFromRankFile(rank int, file string) string {
+	return fmt.Sprintf("%s%d", file, rank)
+}
+
+func getRelativeCoord(rank, file string, direction Direction, n int) (string, bool) {
+	rankInt, err := strconv.Atoi(rank)
+	if err != nil {
+		panic(err)
+	}
+	switch direction {
+	case North:
+		// n ranks north
+		newRank := rankInt + n
+		coord := coordFromRankFile(newRank, file)
+		_, isValid := validCoords[coord]
+		return coord, isValid
 	}
 	return "", false
 }
@@ -50,21 +154,16 @@ func CanPawnMove(model Model, squareName string) []string {
 	if isCoordStartPosition(playerColor, Pawn, rank, file) {
 
 		// is one space ahead vacant?
-		rankOneAhead, oneAheadExists := getRankAhead(rank, 1)
-		if oneAheadExists {
-			// is rankOneAhead occupied?
-			_, isOccupied := model.BoardState[file+rankOneAhead]
-			if !isOccupied {
-				validDestinations = append(validDestinations, file+rankOneAhead)
+		if relCoord, isValid := getRelativeCoord(rank, file, North, 1); isValid {
+			if _, isOccupied := model.BoardState[relCoord]; !isOccupied {
+				validDestinations = append(validDestinations, relCoord)
 			}
 		}
 
 		// is two spaces ahead vacant?
-		rankTwoAhead, twoAheadExists := getRankAhead(rank, 2)
-		if twoAheadExists {
-			_, isOccupied := model.BoardState[file+rankTwoAhead]
-			if !isOccupied {
-				validDestinations = append(validDestinations, file+rankTwoAhead)
+		if relCoord, isValid := getRelativeCoord(rank, file, North, 2); isValid {
+			if _, isOccupied := model.BoardState[relCoord]; !isOccupied {
+				validDestinations = append(validDestinations, relCoord)
 			}
 		}
 	}
