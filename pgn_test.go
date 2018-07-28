8843f47e0fdfa3e44696ff66cab8fd8a7132d5ba
Michael Moore <michael.moore@contrastsecurity.com>
2018-07-28 12:56:46 -0400

    :ok_hand: add full pgn example

--- pgn_test.go
+++ pgn_test.go
@@ -8,6 +8,14 @@ import (
 )
 
 /*
+[Event "F/S Return Match"]
+[Site "Belgrade, Serbia JUG"]
+[Date "1992.11.04"]
+[Round "29"]
+[White "Fischer, Robert J."]
+[Black "Spassky, Boris V."]
+[Result "1/2-1/2"]
+
 1. e4 e5 2. Nf3 Nc6 3. Bb5 a6 {This opening is called the Ruy Lopez.}
 4. Ba4 Nf6 5. O-O Be7 6. Re1 b5 7. Bb3 d6 8. c3 O-O 9. h3 Nb8 10. d4 Nbd7
 11. c4 c6 12. cxb5 axb5 13. Nc3 Bb7 14. Bg5 b4 15. Nb1 h6 16. Bh4 c5 17. dxe5
