dd9ef7b6c206e8c9110555899a3e49987cd0ab84
Michael Moore <michael.moore@contrastsecurity.com>
2019-02-16 10:09:22 -0500

    update color reference via api

--- draw.go
+++ draw.go
@@ -7,9 +7,8 @@ import (
 )
 
 type pieceDrawer struct {
-	win   *pixelgl.Window
-	black PieceSpriteSet
-	white PieceSpriteSet
+	win               *pixelgl.Window
+	pieceSpriteSheets map[chessapi.PlayerColor]PieceSpriteSet
 }
 
 // NewPieceDrawer builds a pieceDrawer instance that is responsible for drawing chess pieces
@@ -22,21 +21,23 @@ func NewPieceDrawer(win *pixelgl.Window) (pieceDrawer, error) {
 
 	return pieceDrawer{
 		win: win,
-		black: PieceSpriteSet{
-			King:   newSprite(pic, 0, 0, 40, 40),
-			Queen:  newSprite(pic, 40, 0, 90, 40),
-			Bishop: newSprite(pic, 90, 0, 140, 40),
-			Knight: newSprite(pic, 130, 0, 180, 40),
-			Rook:   newSprite(pic, 185, 0, 220, 40),
-			Pawn:   newSprite(pic, 230, 0, 270, 40),
-		},
-		white: PieceSpriteSet{
-			King:   newSprite(pic, 0, 40, 40, 85),
-			Queen:  newSprite(pic, 40, 40, 90, 85),
-			Bishop: newSprite(pic, 90, 40, 140, 85),
-			Knight: newSprite(pic, 130, 40, 185, 85),
-			Rook:   newSprite(pic, 185, 40, 220, 85),
-			Pawn:   newSprite(pic, 230, 40, 270, 85),
+		pieceSpriteSheets: map[chessapi.PlayerColor]PieceSpriteSet{
+			chessapi.PlayerBlack: PieceSpriteSet{
+				King:   newSprite(pic, 0, 0, 40, 40),
+				Queen:  newSprite(pic, 40, 0, 90, 40),
+				Bishop: newSprite(pic, 90, 0, 140, 40),
+				Knight: newSprite(pic, 130, 0, 180, 40),
+				Rook:   newSprite(pic, 185, 0, 220, 40),
+				Pawn:   newSprite(pic, 230, 0, 270, 40),
+			},
+			chessapi.PlayerWhite: PieceSpriteSet{
+				King:   newSprite(pic, 0, 40, 40, 85),
+				Queen:  newSprite(pic, 40, 40, 90, 85),
+				Bishop: newSprite(pic, 90, 40, 140, 85),
+				Knight: newSprite(pic, 130, 40, 185, 85),
+				Rook:   newSprite(pic, 185, 40, 220, 85),
+				Pawn:   newSprite(pic, 230, 40, 270, 85),
+			},
 		},
 	}, nil
 }
@@ -50,12 +51,8 @@ func (drawer pieceDrawer) Draw(boardState chessapi.BoardState, squares BoardMap)
 
 	// Draw pieces in the correct position
 	for coord, livePieceData := range boardState {
-		var set PieceSpriteSet
-		if livePieceData.Color == chessapi.PlayerBlack {
-			set = drawer.black
-		} else {
-			set = drawer.white
-		}
+
+		set := drawer.pieceSpriteSheets[livePieceData.Color]
 
 		var piece *pixel.Sprite
 		switch livePieceData.Piece {
